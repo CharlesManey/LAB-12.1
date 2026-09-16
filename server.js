acbd8a7a9673d7a111c9364b878d0be88ca25453
Charles <tmaney1990@gmail.com>
2026-09-16 14:56:20 -0400

    initial commit project from scratch with server set up and all paths working correctly

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/public/index.html`);
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/public/contact.html`);
+});
+
+app.get('/menu', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/public/menu.html`);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
